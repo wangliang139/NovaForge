2bc71ac8b6cadf512ba4e94bfa47350d6eecd9d5
liangwang <torch@alva.com>
2026-04-17 14:55:20 +0800

    chore: rename frontend title

--- frontend/config/defaultSettings.ts
+++ frontend/config/defaultSettings.ts
@@ -15,7 +15,7 @@ const Settings: ProLayoutProps & {
   fixedHeader: false,
   fixSiderbar: true,
   colorWeak: false,
-  title: 'BackOffice',
+  title: 'NovaForge',
   pwa: true,
   logo: '/logo.svg',
   iconfontUrl: '',
